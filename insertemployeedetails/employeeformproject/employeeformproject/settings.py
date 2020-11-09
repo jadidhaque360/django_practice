3bbc2868a432e1493b7180636cbaff2487b8396b
jadidhaque360 <62088438+jadidhaque360@users.noreply.github.com>
2020-11-09 17:46:39 +0530

    Update settings.py

--- insertemployeedetails/employeeformproject/employeeformproject/settings.py
+++ insertemployeedetails/employeeformproject/employeeformproject/settings.py
@@ -73,6 +73,12 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'employeeformproject.wsgi.application'
 
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+        'LOCATION': '127.0.0.1:11211',
+    }
+}
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
